6df6efbb9edd2a5a6ee448c8bbb0cc113e69c1ec
ocampojosue <ocampojosue@gmail.com>
2021-01-11 04:25:40 -0400

    crud periods

--- resources/views/Period/create.blade.php
+++ resources/views/Period/create.blade.php
@@ -51,7 +51,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Save Period') }}
+                                    {{ __('Save Periods') }}
                                 </button>
                                 <a href="{{route('period.index')}}" class="btn btn-danger">Back</a>
                             </div>
