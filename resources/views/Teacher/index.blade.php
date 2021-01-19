8dd42e8fc1ffc7f7d941928f7cfc3e901dc1244b
ocampojosue <ocampojosue@gmail.com>
2021-01-19 00:22:46 -0400

    news tables

--- resources/views/Teacher/index.blade.php
+++ resources/views/Teacher/index.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.app')
 @section('content')
-<div class="row">
-  <div class="container"> 
+<div class="container">
+  <div class="text-left">
     <a href="{{route('teacher.create')}}" class="btn btn-success">Add New Teacher</a><br><br>
-    @foreach($teachers as $teacher)
-      <div class="col-sm">
-        <div class="card" style="width: 18rem; margin-top: 70px;">
-          <img  class="card-img-top mx-auto d-block" src="images/{{$teacher->avatar}}" alt="">
-            <div class="card-body">
-              <h5 class="card-title text-center">{{$teacher->name}} {{$teacher->lastname}}</h5>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item"><strong>Address: </strong>{{$teacher->address}}</li>
-                <li class="list-group-item"><strong>Matter: </strong>{{$teacher->matter}}</li>
-                <li class="list-group-item"><strong>City: </strong>{{$teacher->city}}</li>
-                <li class="list-group-item"><strong>Phone: </strong>{{$teacher->phone}}</li>
-                <li class="list-group-item"><strong>Sex: </strong>{{$teacher->sex}}</li> 
-              </ul>
-              <a href="{{route ('teacher.show',$teacher->id)}}" style="margin: 10px;" class="btn btn-primary mx-auto d-block">Show More...</a>
-            </div>
-            <div class="card-footer">git init
-              <small class="text-muted">Last update: {{$teacher->updated_at}}</small>
-            </div>
+  </div>
+  <div class="row" style="font-size: 15px">
+  @foreach($teachers as $teacher)
+        <div class="col-sm-4" >
+          <div class="card" style="width: 16rem;">
+            <img  class="card-img-top mx-auto d-block" src="images/{{$teacher->avatar}}" alt="" style="width: 200px;height:200px;">
+              <div class="card-body" style="padding: 0;margin:7px 0px">
+                <h5 class="card-title text-center">{{$teacher->name}} {{$teacher->lastname}}</h5>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><strong>Address: </strong>{{$teacher->address}}</li>
+                  <li class="list-group-item"><strong>Matter: </strong>{{$teacher->matter}}</li>
+                  <li class="list-group-item"><strong>City: </strong>{{$teacher->city}}</li>
+                  <li class="list-group-item"><strong>Phone: </strong>{{$teacher->phone}}</li>
+                  <li class="list-group-item"><strong>Sex: </strong>{{$teacher->sex}}</li> 
+                  <li class="list-group-item">
+                    <a href="{{route ('teacher.show',$teacher->id)}}" style="" class="btn btn-primary mx-auto d-block">
+                      Show More...
+                    </a>
+                  </li> 
+                </ul>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Last update: {{$teacher->updated_at}}</small>
+              </div>
+          </div><br><br>
         </div>
-      </div>
-    @endforeach
+      @endforeach
   </div>
 </div>
 @endsection
